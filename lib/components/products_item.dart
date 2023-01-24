56420685b5a4da93bcea5af09ce3badbcdfd5c0e
LisRodrigues <lisrodriguss@gmail.comgit config --global user.email lisrodriguss@gmail.comgit config --global user.email lisrodriguss@gmail.com>
2023-01-24 11:58:52 -0300

    Criação de componente ItemProduto e estilização

--- lib/components/products_item.dart
+++ lib/components/products_item.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:ger_estado/models/produtos.dart';
 
@@ -8,6 +10,34 @@ class ProdutoItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(product.title);
+    return ClipRRect(
+      //arredondar o retangulo
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        // ignore: sort_child_properties_last
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.start,
+            style: TextStyle(fontSize: 12),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //final theme = ThemeData();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+              .copyWith(secondary: Colors.deepOrange),
+          fontFamily: 'Lato'),
       home: ProdutosOverviewPage(),
       debugShowCheckedModeBanner: false,
     );
