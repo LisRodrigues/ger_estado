588e8968b587114f81a757f4a42a417b8a87eef8
LisRodrigues <lisrodriguss@gmail.comgit config --global user.email lisrodriguss@gmail.comgit config --global user.email lisrodriguss@gmail.com>
2023-01-24 10:24:57 -0300

    first commit

---
+++ lib/models/produtos.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, camel_case_types
+class produto {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  produto({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isFavorite = false,
+  });
+
+  void toggleFavorite() {
+    isFavorite = !isFavorite;
+  }
+}
---
+++ lib/pages/podutos_overview_page.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:ger_estado/data/dummy_data.dart';
+import 'package:ger_estado/models/produtos.dart';
+
+class ProdutosOverviewPage extends StatelessWidget {
+  final List<produto> loadedProdutos = dummyProdutos;
+
+  ProdutosOverviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Minha loja")),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          //é preciso dizer a quantidade de itens que ele vai renderizar
+          itemCount: loadedProdutos.length,
+          //Diz como vai ser construida casa coisa do gridview
+          itemBuilder: (ctx, i) => Text(loadedProdutos[i].title),
+          //slivergride = Quando vc tem uma área dentro de algo rolável com uma quantidade fixa na linha. Basicamente, ele define uma estrutura com x elementos em cada linha
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              //Quantidade de itens por linha
+              crossAxisCount: 2,
+              //dimensão de cada elemento.Relação entre altura e largura
+              childAspectRatio: 3 / 2,
+              //espaçamento entre os itens
+              crossAxisSpacing: 10,
+              //espaçamento no eixo principal
+              mainAxisSpacing: 10),
+        ),
+      ),
+    );
+  }
+}
